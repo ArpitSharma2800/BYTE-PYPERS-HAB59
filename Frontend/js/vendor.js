a4466d98492baf7e3ac480cddff055cd77070a5e
atimabh <abarunaha@gmail.com>
2020-08-18 17:36:36 +0530

    final

--- Frontend/js/vendor.js
+++ Frontend/js/vendor.js
@@ -1,4 +1,6 @@
 document.getElementById('productSubmit').addEventListener('click', (e) => {
+
+    console.log(document.getElementById('imgupload'))
     e.preventDefault();
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -7,26 +9,47 @@ document.getElementById('productSubmit').addEventListener('click', (e) => {
     // var quantity = document.getElementById('prodQuantity').value;
     // var price = document.getElementById('prodPrice').value;
 
-    var raw = JSON.stringify({
-        nameOfProduct: document.getElementById('prodName').value,
-        quantity: document.getElementById('prodQuantity').value,
-        price: document.getElementById('prodPrice').value
-    })
-
+    var x = document.getElementById('imgupload');
+    var formdata = new FormData();
+    formdata.append("image", x.files[0], document.getElementById('imgupload').value);
+    formdata.append("nameOfProduct", document.getElementById('prodName').value);
+    formdata.append("quantity", document.getElementById('prodQuantity').value);
+    formdata.append("price", document.getElementById('prodPrice').value);
 
     var requestOptions = {
+        mode: 'no-cors',
         method: 'POST',
-        headers: myHeaders,
-        body: raw,
+        body: formdata,
         redirect: 'follow'
     };
 
-    fetch("https://byte-pypers.herokuapp.com/list_items", requestOptions)
+    fetch("https://byte-pypers.herokuapp.com/imagee", requestOptions)
         .then(response => response.text())
-        .then(result => {
-            alert('Product Added !!');
-        }).then(() => {
-            document.getElementById('addProd').reset();
-        })
+        .then(result => console.log(result))
         .catch(error => console.log('error', error));
+
+    //     var raw = JSON.stringify({
+    //         nameOfProduct: document.getElementById('prodName').value,
+    //         quantity: document.getElementById('prodQuantity').value,
+    //         price: document.getElementById('prodPrice').value,
+    //         image: document.getElementById('imgupload').value
+    //     })
+
+
+    //     var requestOptions = {
+    //         mode: 'no-cors',
+    //         method: 'POST',
+    //         headers: myHeaders,
+    //         body: raw,
+    //         redirect: 'follow'
+    //     };
+
+    //     fetch("https://byte-pypers.herokuapp.com/imagee", requestOptions)
+    //         .then(response => response.text())
+    //         .then(result => {
+    //             console.log(result);
+    //             alert('Product Added !!');
+    //             document.getElementById('addProd').reset();
+    //         })
+    //         .catch(error => console.log('error', error));
 })
--- Frontend/js/vendorsidelisting.js
+++ Frontend/js/vendorsidelisting.js
@@ -17,19 +17,28 @@ fetch("https://byte-pypers.herokuapp.com/list_items_get", requestOptions)
         console.log(listingResult.prod);
         var delbtnlength = listingResult.prod.length;
 
+
+
         for (var i = 0; i < listingResult.prod.length; i++) {
             var name = listingResult.prod[i].nameOfProduct;
             var price = listingResult.prod[i].price;
             var quantity = listingResult.prod[i].quantity;
+            var id = listingResult.prod[i]._id;
             console.log(name)
             console.log(price)
             console.log(quantity)
 
-            var listingContent = `<tbody><tr id="list${i+1}"><th scope="row">${i+1}</th><td>${name}</td><td>${quantity}</td><td>${price}</td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Drop Price</button></td><td><button type="button" class="btn btn-danger" id="${i+1}">Delete Item</button></td></tr></tbody>`
+
+            var listingContent = `<tbody><tr id="list${i+1}"><th scope="row">${i+1}</th><td>${name}</td><td>${quantity}</td><td>${price}</td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Drop Price</button></td><td><button type="button" class="btn btn-danger" id="${id}" onClick="clickHandler(id)">Delete Item</button></td></tr></tbody>`
+
 
             dynamicListing.insertAdjacentHTML('beforeend', listingContent);
 
         }
+
+
+
+
         return delbtnlength;
     })
     .catch(error => console.log('error', error));
