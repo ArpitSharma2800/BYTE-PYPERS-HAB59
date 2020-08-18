56578ebc84aa586f7cfd06ee0c59fd8972e0d276
sd2001 <im.swarnabha2001@gmail.com>
2020-08-18 11:39:09 +0530

    node_modules

--- Data Analysis/get_database.py
+++
@@ -1,9 +0,0 @@
-import pymongo as p
-
-client=p.MongoClient("mongodb://localhost:27017/")
-
-db=client['vendor']
-details=db.details
-print(client.list_database_names())
-for d in details.find():
-    print(d['password'])
